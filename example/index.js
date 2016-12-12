d4e4483f46eb0d549b081982b8e933b9aa9aa0ee
jonrobins <jonrobins@outlook.com>
2016-12-12 10:21:18 +0000

    updated error messages

--- example/index.js
+++ example/index.js
@@ -16,7 +16,6 @@ hutch.on('ready', function() {
   setup();
 });
 
-// with this removed the "close" event is not emitted and no retry occurs when rabbit gets bounced
 hutch.on('error', function (err) {
   console.log("Error: " + err);
 });
